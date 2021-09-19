396e4abacb810f89b1ed936de16eb50ca193be72
NullNRG <63321722+NullNRG@users.noreply.github.com>
2021-09-19 12:52:28 -0700

    Update IController.cs

--- WitchyTwitchy/IController.cs
+++ WitchyTwitchy/IController.cs
@@ -7,22 +7,5 @@ namespace WitchyTwitchy
 {
     internal interface IController
     {
-  private static string _name;
-  private static string _version;
-  private static InputSimulator sim = new InputSimulator();
-
-        TwitchBot TwitchBot { get; }
-
-        public Task ParseInput();
-        public static void OnMessage(object sender, TwitchChatMessage twitchChatMessage)
-        {
-        }
-
-        public async Task GameInput(VirtualKeyCode key, TimeSpan span, int ms)
-        {
-            await Task.Run(() => sim.Keyboard.KeyDown(key).Sleep(ms));
-            await Task.Delay(span);
-            await Task.Run(() => sim.Keyboard.KeyUp(key));
-        }
     }
 }
